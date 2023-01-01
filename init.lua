74c0e11f36b98eff4b3d79d4d61df21a7bd3123d
Michael Cortese <michael.cortese@ryerson.ca>
2023-01-01 16:42:25 -0500

    enable golang autoformat/import

--- init.lua
+++ init.lua
@@ -338,7 +338,7 @@ end
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
